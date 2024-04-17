7280c48e2caad93896cb97f0e650a7f76c825b6e
vaibhav shrivastava <59912875+vaibhav9221@users.noreply.github.com>
2024-04-17 14:36:48 +0530

    Update server.js

--- server.js
+++ server.js
@@ -77,4 +77,4 @@ app.delete('/tasks/:id', async (req, res) => {
   }
 });
 
-app.listen(5000, () => console.log('Server running on port 5000'));
+app.listen(10000, () => console.log('Server running on port 10000'));
